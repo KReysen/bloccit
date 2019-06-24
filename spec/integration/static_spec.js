34e3d08369afc22665d5d1091902920504163f7b
KReysen <43965763+KReysen@users.noreply.github.com>
2019-06-23 22:38:51 -0600

    Assignment intro to express complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const baseMarco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -18,6 +19,22 @@ describe("routes : static", () => {
         done();
       });
     });
-
   });
+  describe("GET /", () => {
+    it("should return status code 200", (done) => {
+      request.get(baseMarco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        // expect body of response to be "polo"
+        done();
+      });
+    });
+    it("should contain the string polo", (done) => {
+      request.get(baseMarco, (err, res, body) => {
+        expect(body).toContain("polo");
+        done();
+      });
+    });
+  });
+
 });
+// New method for assignment
