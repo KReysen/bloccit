a20522b06d5cd6f8e78a5e903a9df60f05e8d26b
KReysen <43965763+KReysen@users.noreply.github.com>
2019-06-25 21:23:12 -0600

    assignment RCV complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const baseAbout = "http://localhost:3000/about";
+
 
 describe("routes : static", () => {
 
@@ -15,10 +15,12 @@ describe("routes : static", () => {
       });
     });
   });
+
+
 // #2
-  describe("GET /", () => {
+  describe("GET /about", () => {
     it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
-      request.get(baseAbout, (err, res, body) => {
+      request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us");
         done();
