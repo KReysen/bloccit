ad1220bf062c05ae22051feaea503e3b24e99946
KReysen <43965763+KReysen@users.noreply.github.com>
2019-06-25 21:01:35 -0600

    Added test for /about route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,24 +1,29 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const baseAbout = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
 //#1
   describe("GET /", () => {
-
-//#2
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-
-//#4
         done();
       });
     });
-
   });
+// #2
+  describe("GET /", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(baseAbout, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+  });
+
 });
