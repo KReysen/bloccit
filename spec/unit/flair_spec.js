bf4f041e1387f226b768336fc5fee5e6b2b376f4
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-03 16:10:39 -0600

    created Flair model

--- spec/unit/flair_spec.js
+++ spec/unit/flair_spec.js
@@ -3,7 +3,7 @@ const Topic = require("../../src/db/models").Topic;
 const Flair = require("../../src/db/models").Flair;
 
 describe("Flair", () => {
-
+ // create a new topic and flair before each test
   beforeEach((done) => {
     this.topic;
     this.flair;
@@ -17,7 +17,7 @@ describe("Flair", () => {
 
         Flair.create({
           name: "Snazzy space flair",
-          color: "green",
+          color: "silver",
           topicId: this.topic.id
         })
         .then((flair) => {
@@ -31,5 +31,25 @@ describe("Flair", () => {
       });
     });
 
+  });  // end beforeEach
+// test for create method
+  describe("#create()", () => {
+    it("should create a flair object with a name and color and add it to the assigned topic", (done) => {
+      Flair.create({
+        name: "Great galaxy flair",
+        color: "green",
+        topicId: this.topic.id
+      })
+      .then((flair) => {
+        expect(flair.name).toBe("Great galaxy flair");
+        expect(flair.color).toBe("green");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
   });
+
 });
