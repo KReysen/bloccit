752034c8f2b1b59e4a736b02f535f96f00f54b35
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-03 15:41:51 -0600

    reached Edit, Update

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -11,6 +11,28 @@ module.exports = {
       .catch((err) => {
         callback(err);
       })
-    }
-    
+    },
+
+  getPost(id, callback){
+   return Post.findById(id)
+   .then((post) => {
+     callback(null, post);
+   })
+   .catch((err) => {
+     callback(err);
+   })
+ },
+
+ deletePost(id, callback){
+    return Post.destroy({
+      where: { id }
+    })
+    .then((deletedRecordsCount) => {
+      callback(null, deletedRecordsCount);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+
 }
