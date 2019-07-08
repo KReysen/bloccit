df1b73a9faa45df0dface8c6468a315cb277b7c2
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-07 20:08:33 -0600

    added validation to routes for topics

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -85,6 +85,33 @@ describe("routes : topics", () => {
           }
         );
       });
+// Assignment test for validation
+
+it("should not create a new topic that fails validations", (done) => {
+       const options = {
+         url: `${base}create`,
+         form: {
+           title: "a",
+           description: "b"
+         }
+       };
+
+       request.post(options,
+         (err, res, body) => {
+
+           Topic.findOne({where: {title: "a"}})
+           .then((topic) => {
+               expect(topic).toBeNull();
+               done();
+           })
+           .catch((err) => {
+             console.log(err);
+             done();
+           });
+         }
+       );
+     });
+
     });
 
     describe("POST /topics/:id/destroy", () => {
