a83a75bd2b704ade89eb05a1253e550f1c435d3e
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-09 15:40:44 -0600

    added spec for mock-auth

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,25 +1,33 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    email: {
+  email: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       isEmail: { msg: "must be a valid email" }
     }
   },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-   {});
-  User.associate = function(models) {
-    // associations can be defined here
-    User.hasMany(models.Post, {
-      foreignKey: "userId",
-      as: "posts"
-    });
-  };
-  return User;
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "member"
+  }
+}, {});
+
+User.associate = function(models) {
+ User.hasMany(models.Post, {
+    foreignKey: "userId",
+    as: "posts"
+  });
+};
+
+User.prototype.isAdmin = function() {
+ return this.role === "admin";
+};
+return User;
 };
