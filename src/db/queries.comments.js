6e6007db39cae35b571d3634a148a677e80fc329
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-15 14:32:33 -0600

    merging comments to master

--- src/db/queries.comments.js
+++ src/db/queries.comments.js
@@ -18,6 +18,7 @@ module.exports = {
 
   deleteComment(req, callback){
     return Comment.findById(req.params.id)
+
     .then((comment) => {
       const authorized = new Authorizer(req.user, comment).destroy();
 
